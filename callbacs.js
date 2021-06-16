8d67647fff2951b440a36f5e4664a4e094bd15e8
iprasanthdev <j.prasanth.me98@gamil.com>
2021-06-16 14:37:56 +0530

    callbac js updated with seq function

--- callbacs.js
+++ callbacs.js
@@ -1,5 +1,23 @@
-const print = ()=>{
+user = [{
+    name: "prasanth",
+    age:12
+},{
+    name:"vasanth",
+    age:24
+}]
+const print = (name ,callback)=>{
     setTimeout(()=>{
-        console.log("this is a call back function");
-    },1000)
+        console.log(`hi ${name} this is a call back function`);
+        callback({username:name})
+    },5000)
 }
+const  printdetails=(age)=>{
+    setTimeout(()=>{
+        user.array.forEach(element => {
+           console.log(element.age);           
+        });
+    },2000)
+}
+const seq=print("prasanth",seq=>{
+    console.log(seq);
+});
