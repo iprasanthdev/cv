a44eec7d28b032c0e5392c6aa5889fc1b44933a9
iprasanthdev <j.prasanth.me98@gamil.com>
2021-06-16 15:41:46 +0530

    callbac js updated with seq function and printing the details

--- callbacs.js
+++ callbacs.js
@@ -1,23 +1,34 @@
-user = [{
+const user = [{
     name: "prasanth",
     age:12
-},{
+},
+{
     name:"vasanth",
     age:24
-}]
+}];
 const print = (name ,callback)=>{
     setTimeout(()=>{
-        console.log(`hi ${name} this is a call back function`);
-        callback({username:name})
-    },5000)
+        user.forEach(element => {
+        callback(element);
+    })    
+  },5000)
 }
-const  printdetails=(age)=>{
+const  printdetails=(age,callback)=>{
     setTimeout(()=>{
-        user.array.forEach(element => {
-           console.log(element.age);           
-        });
-    },2000)
+        user.forEach(element => {
+           callback(element)})
+  },2000)
 }
-const seq=print("prasanth",seq=>{
-    console.log(seq);
-});
+const seq = print(user,(seq) =>{
+    // console.log(seq)
+    let data = seq;
+    console.log(`hi ${data.name} this is a call back function`);
+    console.log(`username:${data.name}`);
+    console.log("user age: ",data.age);
+    //   printdetails(user,userage=>{
+    //     let data = userage;
+    //     console.log(data);
+    //     console.log(data.age);
+    // });
+}
+);
