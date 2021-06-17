2f0b753f7d7b86960de10da79b6605a65e3c629d
iprasanthdev <j.prasanth.me98@gamil.com>
2021-06-17 21:38:53 +0530

    1st commit

---
+++ whatsapp-clone/src/fire.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAE4AkTIvaFdUYRWaj6grKc1KDnyscsNtY",
+    authDomain: "clone-f9348.firebaseapp.com",
+    projectId: "clone-f9348",
+    storageBucket: "clone-f9348.appspot.com",
+    messagingSenderId: "132074352001",
+    appId: "1:132074352001:web:7fb3ea32f3533a8acacc80",
+    measurementId: "G-N9SCPZDV4E"
+  };
